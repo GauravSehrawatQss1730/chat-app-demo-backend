b6d28fd84ba5d95d0fd43344c836e73f70f63788
Ayush Gupta <ayush.gupta@qsstechnosoft.com>
2025-01-01 20:11:47 +0530

    added mongo db string

--- db/config.js
+++ db/config.js
@@ -3,14 +3,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/chat', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://ayush:ayush@cluster0.ipbsj.mongodb.net/chat?retryWrites=true&w=majority&appName=Cluster0');
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
-    process.exit(1);  // Exit the process if connection fails
+    process.exit(1); // Exit the process if connection fails
   }
 };
 
